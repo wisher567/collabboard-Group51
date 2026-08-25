257f4f724a19d824b70ef3c14b4d44c38ce947cb
poorna dhananjana <poornadhananjna@gmail.com>
2026-08-25 13:05:42 +0530

    Wire up MongoDB connection in server entrypoint

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const cors = require('cors');
 
 const { notFoundHandler, errorHandler } = require('./middleware/errorHandler');
-
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+connectDB();
 app.use(cors());
 app.use(express.json());
 
